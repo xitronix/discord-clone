b4095391c57e782c95fa773dda3219b14dedd851
Jarek <yaraslau.s@gmail.com>
2023-10-06 00:09:55 +0200

    style: create server dialog

--- app/(protected)/page.tsx
+++ app/(protected)/page.tsx
@@ -1,9 +1,24 @@
-export default function Home() {
-  return (
-    <div className="flex flex-col">
-      <p className="text-3xl font-bold">
-        This is protected route
-      </p>
-    </div>
-  );
-}
+import { initialProfile } from "@/lib/initialProfile";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+import { InitialModal } from "@/components/modals/InitialModal";
+
+const SetupPage = async () => {
+  const profile = await initialProfile();
+
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (server) return redirect(`/servers/${server.id}`);
+
+  return <InitialModal />;
+};
+
+export default SetupPage;
---
+++ components/modals/InitialModal.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import { DialogTitle } from "@radix-ui/react-dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+} from "../ui/dialog";
+import zod from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormMessage,
+  FormLabel,
+  FormItem,
+  FormField,
+  FormControl,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
+
+const formSchema = zod.object({
+  name: zod.string().min(1, "Server name is required"),
+  imageUrl: zod.string().min(1, "Server image is required"),
+});
+
+export const InitialModal = () => {
+  const [isMounted, setIsMounted] = useState(false);
+  const [isOpen, setIsOpen] = useState(true);
+
+  useEffect(() => setIsMounted(true), []);
+  const form = useForm({
+    resolver: zodResolver(formSchema, { async: true }),
+    defaultValues: {
+      name: "",
+      imageUrl: "",
+    },
+  });
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: zod.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <Dialog open={isOpen} onOpenChange={(isOpen) => setIsOpen(isOpen)}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle className="text-center text-bold">
+            Create server
+          </DialogTitle>
+          <DialogDescription className="text-center">
+            Customize your server with a name and an picture. You can change it
+            later.
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form className="" onSubmit={form.handleSubmit(onSubmit)}>
+            <div className="flex items-center justrify-center text-center pb-6">
+              TODO: Imgage Upload
+            </div>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="uppercase text-xs font-bolddark:text-zinc-500">
+                    Server Name
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      className="dark:bg-zinc-600/50 border-0 focus-visible:ring-0 focus-visible:ring-offset-0"
+                      placeholder="Enter server name"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            ></FormField>
+            <DialogFooter className="pt-6">
+              <Button type="submit" disabled={isLoading} variant="default">
+                Create
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
