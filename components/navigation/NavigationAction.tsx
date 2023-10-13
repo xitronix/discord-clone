8b2cf0265b7b3cb70281ef92592d5133fc1f92e0
Jarek <yaraslau.s@gmail.com>
2023-10-13 13:25:11 +0200

    fix: browser error

--- components/navigation/NavigationAction.tsx
+++ components/navigation/NavigationAction.tsx
@@ -7,14 +7,14 @@ const NavigationAction = () => {
   return (
     <div>
       <ActionTooltip side="right" align="center" label="add a server">
-        <button className="group">
+        <div className="group">
           <div className="flex mx-3 h-12 w-12 rounded-3xl group-hover:rounded-2xl transition-all overflow-hidden justify-center items-center bg-background group-hover:bg-emerald-500">
             <Plus
               className="group-hover:text-foreground transition text-emerald-700"
               size={25}
             />
           </div>
-        </button>
+        </div>
       </ActionTooltip>
     </div>
   );
