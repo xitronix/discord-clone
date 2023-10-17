9167b7e9caf60a9bd062bc3b0bff40c1921ac46a
Jarek <yaraslau.s@gmail.com>
2023-10-18 00:54:45 +0200

    refactor: remove unused code

--- app/api/servers/[serverId]/route.ts
+++ app/api/servers/[serverId]/route.ts
@@ -1,7 +1,6 @@
 import { currentProfile } from "@/lib/currentProfile";
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
-import { v4 as uuidv4 } from "uuid";
 
 export const PATCH = async (
   req: Request,
