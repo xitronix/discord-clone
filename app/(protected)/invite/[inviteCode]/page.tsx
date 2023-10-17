01eacc5911e690805b93c038d50a08a861a92c5d
Jarek <yaraslau.s@gmail.com>
2023-10-17 14:58:21 +0200

    feat: invite page

---
+++ app/(protected)/invite/[inviteCode]/page.tsx
@@ -0,0 +1,43 @@
+import { currentProfile } from "@/lib/currentProfile";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+
+export const InvitePage = async ({
+  params: { inviteCode },
+}: {
+  params: { inviteCode: string };
+}) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirect("/login");
+  }
+  const memberedServer = await db.server.findFirst({
+    where: {
+      inviteCode,
+      members: {
+        some: { profileId: profile.id },
+      },
+    },
+  });
+  if (memberedServer) {
+    return redirect(`/servers/${memberedServer.id}`);
+  }
+
+  const server = await db.server.update({
+    where: {
+      inviteCode,
+    },
+    data: {
+      members: {
+        create: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  return redirect(`/servers/${server.id}`);
+};
+
+export default InvitePage;
