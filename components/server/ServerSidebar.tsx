0f53240174a3713969f1e866d303d66236b41ec9
Jarek <yaraslau.s@gmail.com>
2023-10-19 14:52:29 +0200

    feat: show channels

--- components/server/ServerSidebar.tsx
+++ components/server/ServerSidebar.tsx
@@ -3,6 +3,7 @@ import { db } from "@/lib/db";
 import { Channel, ChannelType } from "@prisma/client";
 import { redirect } from "next/navigation";
 import { ServerHeader } from "./ServerHeader";
+import { ChannelIcon } from "../ChannelIcon";
 
 interface ServerSidebarProps {
   serverId: string;
@@ -29,20 +30,20 @@ export const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
 
   const channels = server?.channels.reduce<{
     text: Channel[];
-    audio: Channel[];
+    voice: Channel[];
     video: Channel[];
   }>(
     (prevValue, curr) => {
       if (curr.type === ChannelType.TEXT) {
         prevValue.text.push(curr);
-      } else if (curr.type === ChannelType.AUDIO) {
-        prevValue.audio.push(curr);
+      } else if (curr.type === ChannelType.VOICE) {
+        prevValue.voice.push(curr);
       } else if (curr.type === ChannelType.VIDEO) {
         prevValue.video.push(curr);
       }
       return prevValue;
     },
-    { text: [], audio: [], video: [] }
+    { text: [], voice: [], video: [] }
   );
 
   const members = server?.members.filter(
@@ -58,8 +59,32 @@ export const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
   }
 
   return (
-    <div className="flex flex-col w-full h-full text-foreground dark:bg-[#2B2D31] bg-[#F2F3F5]">
+    <div className="flex flex-col w-full h-full p-0 text-foreground dark:bg-[#2B2D31] bg-[#F2F3F5]">
       <ServerHeader server={server} role={role} />
+      <div className="p-2">
+        <Channels title="Text channels" channels={channels?.text} />
+        <Channels title="Voice channels" channels={channels?.voice} />
+        <Channels title="Video channels" channels={channels?.video} />
+      </div>
+    </div>
+  );
+};
+
+const Channels = ({
+  channels,
+  title,
+}: {
+  channels?: Channel[];
+  title: string;
+}) => {
+  return (
+    <div>
+      <div className="uppercase text-xs font-bold pt-4">{title}</div>
+      {channels?.map(({ name, id, type }) => (
+        <div key={id} className="flex py-1 px-2 gap-2">
+          <ChannelIcon className="h-4 w-4" type={type} /> {name}
+        </div>
+      ))}
     </div>
   );
 };
