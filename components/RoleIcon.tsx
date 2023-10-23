13b108721eec668c3a691d19a1f44063a6d49ea4
Jarek <yaraslau.s@gmail.com>
2023-10-23 11:34:19 +0200

    feat: add classname to role icon

--- components/RoleIcon.tsx
+++ components/RoleIcon.tsx
@@ -1,15 +1,17 @@
+import { cn } from "@/lib";
 import { MemberRole } from "@prisma/client";
 import { CrownIcon, ShieldCheck } from "lucide-react";
 
 interface RoleIconProps {
   role: string;
+  className: string;
 }
 
-export const RoleIcon = ({ role }: RoleIconProps) => {
+export const RoleIcon = ({ role, className }: RoleIconProps) => {
   if (role === MemberRole.MODERATOR) {
-    return <ShieldCheck className="h-5 w-5" />;
+    return <ShieldCheck className={cn("h-5 w-5", className)} />;
   } else if (role === MemberRole.ADMIN) {
-    return <CrownIcon className="h-4 w-4" />;
+    return <CrownIcon className={cn("h-4 w-4", className)} />;
   }
   return null;
 };
