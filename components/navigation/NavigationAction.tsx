5c97b8e23d668676055675d2034a5e90137b7f0a
Jarek <yaraslau.s@gmail.com>
2023-10-23 14:04:46 +0200

    style(NavigationAction): adjust green/white colors

--- components/navigation/NavigationAction.tsx
+++ components/navigation/NavigationAction.tsx
@@ -10,9 +10,9 @@ const NavigationAction = () => {
     <div onClick={() => onOpen("createServer")}>
       <ActionTooltip side="right" align="center" label="add a server">
         <div className="group">
-          <div className="flex mx-3 h-12 w-12 rounded-3xl group-hover:rounded-2xl transition-all overflow-hidden justify-center items-center bg-background group-hover:bg-emerald-500">
+          <div className="flex mx-3 h-12 w-12 rounded-3xl group-hover:rounded-2xl transition-all overflow-hidden justify-center items-center bg-background group-hover:bg-green-600">
             <Plus
-              className="group-hover:text-foreground transition text-emerald-700"
+              className="group-hover:text-foreground transition text-green-600 group-hover:text-white"
               size={25}
             />
           </div>
