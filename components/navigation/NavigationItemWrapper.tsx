5daa4ef1d4ff8096f5bdd2d711d86f49c8c7cf97
Jarek <yaraslau.s@gmail.com>
2023-11-02 22:20:30 +0100

    refactor: remove unused code

--- components/navigation/NavigationItemWrapper.tsx
+++ components/navigation/NavigationItemWrapper.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { cn } from "@/lib";
-import { useParams, usePathname } from "next/navigation";
 
 export const NavigationActionWrapper = ({
   children,
@@ -10,12 +9,6 @@ export const NavigationActionWrapper = ({
   children: React.ReactNode;
   className?: string;
 }) => {
-  // cn(
-  //   "relative flex mx-3 h-12 w-12 rounded-3xl group-hover:rounded-2xl transition-all overflow-hidden justify-center items-center",
-  //   params?.serverId === id && "bg-primary/10 text-primary rounded-2xl"
-  // )}
-  const pathname = usePathname();
-  console.log(pathname);
   return (
     <div className="group">
       <div
